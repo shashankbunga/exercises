4e00167cb803f0aafffc7539758898315e486922
shashankbunga <S.Bunga2@newcastle.ac.uk>
2022-11-12 02:13:56 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,5 @@
+Birth_year = input("enter your birth year")
+age = 2022- int(Birth_year)
+print (age)
+
+float()
---
+++ pet.py
@@ -0,0 +1,5 @@
+class Pet:
+    def __init__(self, name, animal_type, age):
+        self.name = name
+        self.animal_type = animal_type
+        self.age = age
